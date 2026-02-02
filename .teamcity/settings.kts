a768aff150acf11fe92bfb31802ea2f682e48e2e
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-02-02 16:40:26 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.pipelines.*
+//import jetbrains.buildServer.configs.kotlin.pipelines.*
 
 /*
 The settings script is an entry point for defining a TeamCity
