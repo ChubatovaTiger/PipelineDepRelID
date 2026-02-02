47c675e9638004cb69ec930703465e78209fdb76
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-02-02 16:26:46 +0100

    Update settings.kts

--- .teamcityPpl/settings.kts
+++ .teamcityPpl/settings.kts
@@ -1,6 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-
+import jetbrains.buildServer.configs.kotlin.pipelines.*
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
